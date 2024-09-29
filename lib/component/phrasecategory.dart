f2e69a7c157e42320830c02508f868f47d3065b7
Alaa Mahrous <alamhrws8@gmail.com>
2024-09-29 12:41:43 +0300

    last

--- lib/component/phrasecategory.dart
+++ lib/component/phrasecategory.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: must_be_immutable, avoid_unnecessary_containers
 
-import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:the_back/models/item.dart';
 
